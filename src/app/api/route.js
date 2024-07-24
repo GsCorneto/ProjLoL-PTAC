9b60377894d251b13df5d601603fbd7427cfedea
GuiCorneto <131817217+GsCorneto@users.noreply.github.com>
2024-07-23 22:41:54 -0400

    Finalizado Rota Dinâmica
    Rota Dinamica para grande parte dos items está funcionando, item id 1 não apresenta funcionamento ideal.

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -4,7 +4,7 @@ export const listaDeChamps = [
        {
         id: 1,
         nome: "Amumu",
-        funcao: ["Tanque", "Suporte"],
+        funcao: "Tanque",
         preco: 950,
         imagem: "https://www.mobafire.com/images/social/build-card/amumu.jpg",
         descricao: "Reza a lenda que Amumu é uma alma solitária e melancólica da antiga Shurima que vaga sem rumo pelo mundo em busca de um amigo.",
@@ -13,7 +13,7 @@ export const listaDeChamps = [
        {
         id: 2,
         nome: "Sett",
-        funcao: ["Tanque", "Lutador"],
+        funcao: "Lutador",
         preco: 2350,
         imagem: "https://www.mobafire.com/images/social/build-card/sett.jpg",
         descricao: "Líder de parte do próspero submundo do crime em Ionia, Sett deve todo o seu sucesso à guerra contra Noxus.",
@@ -22,7 +22,7 @@ export const listaDeChamps = [
        {
         id: 3,
         nome: "Rengar",
-        funcao: ["Caçador", "Assassino"],
+        funcao: "Caçador",
         preco: 950,
         imagem: "https://www.mobafire.com/images/social/build-card/rengar.jpg",
         descricao: "Rengar é um feroz caçador de troféus vastaya que gosta da emoção proporcionada pela caça e pelo abate de criaturas perigosas.",
@@ -31,7 +31,7 @@ export const listaDeChamps = [
        {
         id: 4,
         nome: "Jhin",
-        funcao: ["Atirador", "Mago"],
+        funcao: "Atirador",
         preco: 4444,
         imagem: "https://www.mobafire.com/images/social/build-card/jhin.jpg",
         descricao: "Jhin é um meticuloso psicopata criminoso que acredita que o assassinato é uma arte.",
@@ -40,7 +40,7 @@ export const listaDeChamps = [
        {
         id: 5,
         nome: "Thresh",
-        funcao: ["Suporte", "Tanque"],
+        funcao: "Suporte",
         preco: 950,
         imagem: "https://www.mobafire.com/images/social/build-card/thresh.jpg",
         descricao: "Sádico e astuto, Thresh é um espírito ambicioso e incansável das Ilhas das Sombras. Ele já foi guardião de incontáveis segredos arcanos, mas acabou sucumbindo a poderes maiores do que a vida e a morte.",
@@ -49,7 +49,7 @@ export const listaDeChamps = [
        {
         id: 6,
         nome: "Ornn",
-        funcao: ["Tanque"],
+        funcao: "Tanque",
         preco: 950,
         imagem: "https://www.mobafire.com/images/social/build-card/ornn.jpg",
         descricao: "Ornn é o espírito Freljordano da forja e da artesania. Ele trabalha na solidão de uma enorme fornalha, construída a marteladas por entre as cavernas de lava do vulcão Pedra-Lar.",
@@ -58,7 +58,7 @@ export const listaDeChamps = [
        {
         id: 7,
         nome: "Ezreal",
-        funcao: ["Atirador", "Mago"],
+        funcao: "Atirador",
         preco: 950,
         imagem: "https://www.mobafire.com/images/social/build-card/ezreal.jpg",
         descricao: "Um aventureiro arrojado, com um talento nato nas artes mágicas, Ezreal desbrava catacumbas soterradas, envolve-se com antigas maldições e supera com facilidade as situações mais improváveis.",
@@ -67,7 +67,7 @@ export const listaDeChamps = [
        {
         id: 8,
         nome: "Kayn",
-        funcao: ["Lutador", "Assassino"],
+        funcao:  "Assassino",
         preco: 2350,
         imagem: "https://www.mobafire.com/images/social/build-card/kayn.jpg",
         descricao: "Inigualável praticante da letal magia sombria, Shieda Kayn luta para alcançar seu verdadeiro destino: um dia comandar a Ordem das Sombras rumo a uma nova era de supremacia ioniana.",
@@ -76,7 +76,7 @@ export const listaDeChamps = [
        {
         id: 9,
         nome: "Aatrox",
-        funcao: ["Lutador"],
+        funcao: "Lutador",
         preco: 2350,
         imagem: "https://www.mobafire.com/images/social/build-card/aatrox.jpg",
         descricao: "Antes defensores honrados de Shurima contra o temido Vazio, Aatrox e seus irmãos acabaram se tornando uma ameaça ainda maior para Runeterra, e a única coisa capaz de derrotá-los foi um tipo de feitiçaria mortal e traiçoeira.",
@@ -85,7 +85,7 @@ export const listaDeChamps = [
        {
         id: 10,
         nome: "Shen",
-        funcao: ["Tanque"],
+        funcao: "Tanque",
         preco: 950,
         imagem: "https://www.mobafire.com/images/social/build-card/shen.jpg",
         descricao: "Para os guerreiros ionianos sigilosos conhecidos como Kinkou, Shen, o Olho do Crepúsculo, é um líder. Ele busca permanecer livre da confusão das emoções, preconceito ou ego, caminhando pelo caminho nunca visto do julgamento imparcial entre o reino dos espiritos",
--- src/app/campeao/[id]/page.js
+++ src/app/campeao/[id]/page.js
@@ -1,11 +1,29 @@
-
-export default async function Product({ params }){
+import styles from "../../components/comps.module.css"
+import Link from "next/link";
+export default async function Campeao({ params }){
     const response = await fetch("http://localhost:3000/api/" + params.id);
     const data = await response.json();
 
       return(
-        <div key={data.id}>
-            <p>{data.nome}</p>
+        <main className={styles.pageID}>
+          <div className={styles.campeaoInfo}>
+          <h1>{data.nome}</h1>
+          <img src={data.imagem} alt={data.nome} />
+          <p className={styles.descricao}>{data.descricao}</p>
+          <div className={styles.atributos}>
+            <div className={styles.atributo}>
+              <h3>Função</h3>
+              <p>{data.funcao}</p>
+            </div>
+            <div className={styles.atributo}>
+              <h3>Dificuldade</h3>
+              <p>{data.dific}</p>
+            </div>
+          </div>
+          <div className={styles.preco}>
+            <p>{data.preco} Ciscos Azuis</p>
+          </div>
         </div>
+        </main>
       )
     }
